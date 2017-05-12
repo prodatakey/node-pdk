4f96ec722f9e819c6cf22f1f6290b71cc7d8e23d
Dmitry <dmitry.redkovolosov@dsr-company.com>
2017-05-12 11:42:15 +0300

    Add refresh tokenset to the example

--- src/examples/inventory.js
+++ src/examples/inventory.js
@@ -1,25 +1,25 @@
-import { writeFile } from 'fs';
+import {writeFile} from 'fs';
 import url from 'url';
 import opener from 'opener';
-import authenticate from '../authenticator';
-import makesession from '../session';
-import { getOu, getPanelToken } from '../authApi';
+import {authenticate, refreshTokenSet} from '../authenticator';
+import {makesession} from '../session';
+import {getOu, getPanelToken} from '../authApi';
 import _ from 'lodash/fp';
 import p from 'asyncp';
 
 process.on('unhandledRejection', r => console.log(r));
 
-(async function() {
+(async function () {
   let tokenset = await authenticate(
     process.env.PDK_CLIENT_ID,
     process.env.PDK_CLIENT_SECRET,
     opener,
   );
-  const authsession = makesession(tokenset.id_token);
+  let authsession = makesession(tokenset.id_token);
 
   // Connect to the panel and itemize asset info
   // Panel => InventoriedPanel
-  const inventoryPanel = _.curry(async (authsession, { id, name, uri }) => {
+  const inventoryPanel = _.curry(async(authsession, {id, name, uri}) => {
     // Create an authentication session to the panel's API
     const panelsession = makesession(
       await getPanelToken(authsession, id),
@@ -32,7 +32,7 @@ process.on('unhandledRejection', r => console.log(r));
     try {
       devices = await panelsession('devices');
       connected = true;
-    } catch(err) {
+    } catch (err) {
       //console.log('Unable to negotiate session with panel', err);
     }
 
@@ -50,19 +50,29 @@ process.on('unhandledRejection', r => console.log(r));
 
   // Recursively processes asset info for every panel in an OU and its children OUs
   // OU => InventoriedOU
-  const inventoryOu = _.curry(async (authsession, ouId) => {
-    const { name, owner, panels, children } = await getOu(authsession, ouId);
+  const inventoryOu = _.curry(async(authsession, ouId) => {
+    let ou;
+    try {
+      ou = await getOu(authsession, ouId);
+    } catch (err) {
+      if (err.statusCode === 401) {
+        tokenset = await refreshTokenSet(process.env.PDK_CLIENT_ID,
+          process.env.PDK_CLIENT_SECRET, tokenset.refresh_token);
+        authsession = makesession(tokenset.id_token);
+        ou = await getOu(authsession, ouId);
+      }
+    }
 
-    console.log(`${name}: panels ${panels.length} children ${children.length}`);
+    console.log(`${ou.name}: panels ${ou.panels.length} children ${ou.children.length}`);
 
     // Return the inventoried OU
     return {
-      name,
-      owner,
+      name: ou.name,
+      owner: ou.owner,
       // Inventory each panel in the OU
-      panels: await p.map(panels, inventoryPanel(authsession)),
+      panels: await p.map(ou.panels, inventoryPanel(authsession)),
       // Recurse to inventory any children of this OU
-      children: await p.map(children, _.compose(inventoryOu(authsession), ou => ou.id)),
+      children: await p.map(ou.children, _.compose(inventoryOu(authsession), ou => ou.id)),
     };
   });
 
@@ -70,5 +80,5 @@ process.on('unhandledRejection', r => console.log(r));
   const assets = await inventoryOu(authsession, 'mine');
 
   // Write the inventory out to a file
-  writeFile('./inventory.json', JSON.stringify(assets, null, 2) , 'utf-8');
-}())
+  writeFile('./inventory.json', JSON.stringify(assets, null, 2), 'utf-8');
+}());
