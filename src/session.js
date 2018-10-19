23698dffc75f06d3cca4ece828da43267c41fa89
Joshua Perry <josh@6bit.com>
2018-10-18 18:16:33 -0600

    collaps defaults into context object

--- src/session.js
+++ src/session.js
@@ -79,13 +79,8 @@ export function makeSession(token_set, baseUrl = 'https://accounts.pdk.io/api/')
 // Sugar for setting paging querystring values and communicating paging
 // info back via the query reference parameter
 export const page = async (session, resource, query, callopts = {}) => {
-  // Set defaults in reference object friendly way
-  query.page = query.page || 0
-  query.sort = query.sort || 'asc'
-  query.per_page = query.per_page || 100
-
   // Make the call
-  const resp = await session(resource, { query, ...callopts })
+  const resp = await session(resource, { query: { page: 0, sort: 'asc', per_page: 100, ...query }, ...callopts })
 
   // Set the next values into the paging context so the caller can call in a loop checking `more` on each iteration
   if(resp.link && resp.link.next) {
