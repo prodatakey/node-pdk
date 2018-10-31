131c20b25d44acb0ef54f4a06fb6ef3998bb9c56
Joshua Perry <josh@6bit.com>
2018-10-31 13:36:09 -0600

    Make authstrategy parameter optional

--- src/session.js
+++ src/session.js
@@ -1,10 +1,10 @@
-import got from 'got';
-import url from 'url';
-import Debug from 'debug';
+import got from 'got'
+import url from 'url'
+import Debug from 'debug'
 import parseLink from 'parse-link-header'
 import { authenticate } from './authenticator'
 
-const debug = Debug('pdk:session');
+const debug = Debug('pdk:session')
 
 /**
  * Create a wrapper around the `got` library for simplifying authenticated requests to API endpoints
@@ -16,7 +16,18 @@ const debug = Debug('pdk:session');
  * @param {function} authenticate The authentication strategy to use, currently `authenticator` and `clientauthenticator`.
  * @param {string} baseUrl This base URL used for resolving relative URLs in the endpoint requests.
  */
-export async function makeSession(authopts, authstrategy = authenticate, baseUrl = 'https://accounts.pdk.io/api/') {
+export async function makeSession(authopts, authstrategy, baseUrl = 'https://accounts.pdk.io/api/') {
+
+  // If optional `authstrategy` param is provided as a string
+  // it is meant as the optional `baseUrl` param, so shuffle the args
+  if(typeof(authstrategy) === 'string') {
+    baseUrl = authstrategy
+    authstrategy = undefined
+  }
+
+  // Set authstrategy default if it wasn't defined
+  authstrategy = authstrategy || authenticate
+
   let token_set
   if(typeof(authopts.refresh) === 'function')
     token_set = authopts
@@ -29,59 +40,59 @@ export async function makeSession(authopts, authstrategy = authenticate, baseUrl
     headers: {
       authorization: ''
     }
-  };
+  }
 
   async function freshHeaders() {
-    options.headers.authorization = `Bearer ${(await token_set()).id_token}`;
+    options.headers.authorization = `Bearer ${(await token_set()).id_token}`
   }
 
   // Return an async function that makes a request to an API url and returns the body of the response
   // Simply returns only the body of a resource
-  // person = session('people/123');
+  // person = session('people/123')
   const session = async (resource, callopts) => {
-    let resp;
+    let resp
 
     const call = async (resource, callopts = {}) => (
       await freshHeaders(),
       await got(url.resolve(baseUrl, resource), { ...options, ...callopts })
-    );
+    )
 
     try {
-      debug(`Sending API request ${resource}, ${JSON.stringify(callopts)}`);
-      resp = await call(resource, callopts);
+      debug(`Sending API request ${resource}, ${JSON.stringify(callopts)}`)
+      resp = await call(resource, callopts)
     } catch (err) {
-      debug(`Error from API request ${JSON.stringify(err)}`);
+      debug(`Error from API request ${JSON.stringify(err)}`)
 
       if (err && err.statusCode === 401) {
         // When we get a status 401 we are in need a valid tokenset
         // this can happen for a number of reasons, the token should update optimistically
         // but things like excessive clock skew can throw that off.
 
-        debug(`Forcing token set refresh`);
+        debug(`Forcing token set refresh`)
 
         // We force the token_set to refresh here
-        await token_set.refresh();
+        await token_set.refresh()
 
-        debug(`Retrying API call with fresh token set`);
+        debug(`Retrying API call with fresh token set`)
         // Then retry the call
-        resp = await call(resource, callopts);
+        resp = await call(resource, callopts)
       }
 
       // If we get here then lets rethrow this error for the caller to handle
-      throw err;
+      throw err
     }
 
     // Add a count property for requests with an array body and a total count header
     // This allows the call site to find the total number of paged items available on the server
     if(Array.isArray(resp.body) && 'x-total-count' in resp.headers) {
-      resp.body.count = parseInt(resp.headers['x-total-count']);
+      resp.body.count = parseInt(resp.headers['x-total-count'])
       resp.body.link = parseLink(resp.headers['link'])
     }
 
-    return resp.body;
-  };
+    return resp.body
+  }
 
-  return session;
+  return session
 }
 
 // Sugar for setting paging querystring values and communicating paging
@@ -105,4 +116,4 @@ export const page = async (session, resource, query, callopts = {}) => {
 export default {
   makeSession,
   page
-};
+}
